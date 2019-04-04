5a820075a1717493d7c5213125df22e9fd3a2e43
Barbara <barbaralopezpamias@gmail.com>
2019-04-04 21:07:27 +0200

    assignar grup funcional

--- src/Controller/UsuariController.php
+++ src/Controller/UsuariController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 use App\Entity\Usuari;
+use App\Entity\Grup;
 
 class UsuariController extends AbstractController
 {
@@ -19,18 +20,20 @@ class UsuariController extends AbstractController
     	$user = $this->getUser();
     	$em = $this->getDoctrine()->getManager();
     	$usuaris = $em->getRepository(Usuari::class)->findAll();
+        $grups = $em->getRepository(Grup::class)->findAll();
         $rols = $this->getParameter('security.role_hierarchy.roles');
 
 
         return $this->render('usuari/index.html.twig', [
         	'user' => $user,
             'usuaris' => $usuaris,
+            'grups' => $grups,
             'rols' => $rols,
         ]);
     }
 
     /**
-     * @Route("/assignarRol", name="assignarRol")
+     * @Route("/assignar-rol", name="assignarRol")
      */
     public function assignarRol(Request $request) : JsonResponse {
         
@@ -40,6 +43,23 @@ class UsuariController extends AbstractController
         
         $usuari->removeRole($usuari->getRoles()[0]);
         $usuari->setRoles(array($request->request->get('rol')));
+
+        $em->persist($usuari);
+        $em->flush();
+        
+        return new JsonResponse(['assignat' => true]);
+    }
+
+    /**
+     * @Route("/assignar-grup", name="assignarGrup")
+     */
+    public function assignarGrup(Request $request) : JsonResponse {
+        
+        $em = $this->getDoctrine()->getManager();
+        
+        $usuari = $em->getRepository(Usuari::class)->findOneById($request->request->get('idUsuari'));
+        
+        $usuari->addGrup($em->getRepository(Grup::class)->findOneById($request->request->get('grup')));
 
         $em->persist($usuari);
         $em->flush();
