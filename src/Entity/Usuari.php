d08047395bc509dc8f11219996caebbdf619f5ee
Barbara <barbaralopezpamias@gmail.com>
2019-04-24 19:25:47 +0200

    control d'errors formulari usuari + codi alumne nullable

--- src/Entity/Usuari.php
+++ src/Entity/Usuari.php
@@ -20,7 +20,7 @@ class Usuari extends BaseUser
     protected $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $codiAlumne;
 
